4003c2e8589ac0ac6d6f9289741a1e8d5ebaa49c
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-06-23 19:31:40 +0200

    added app definition

---
+++ app.js
@@ -0,0 +1,24 @@
+Ext.Loader.setConfig({
+    enable: true
+});
+
+Ext.onReady(function () {
+    var dependencies = ['SCE.ClassSystem', 'SCE.Templates', 'SCE.Data', 'SCE.Tree', 'SCE.Chart', 'SCE.Views'];
+
+    Ext.application({
+        name: 'SCE',
+
+        requires: Ext.clone(dependencies),
+
+        launch: function () {
+            var me = this;
+
+            Ext.Msg.prompt('Choice', 'Choose something to execute from the following list: ' + dependencies.join(', '), function (result, value) {
+                value = 'SCE.' + value;
+
+                if (dependencies.indexOf(value) !== -1) Ext.create(value).execute();
+                else Ext.Msg.alert('Error', 'No class name ' + value + ' exists!');
+            });
+        }
+    });
+});
