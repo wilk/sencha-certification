c61046923340caad9378efff057a9a45c8d82327
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-07-01 10:07:42 +0200

    added more examples

--- app/Views.js
+++ app/Views.js
@@ -22,7 +22,7 @@ Ext.define('SCE.Views', {
             '</div>'
         ]);
 
-        var list = Ext.create('Ext.view.View', {
+        Ext.create('Ext.view.View', {
             renderTo: Ext.getBody(),
             store: store,
             tpl: tpl,
@@ -30,6 +30,46 @@ Ext.define('SCE.Views', {
             listeners: {
                 itemclick: function (view, record) {
                     Ext.Msg.alert('Item clicked', record.get('name') + ' has ' + record.get('age') + ' years old!');
+                }
+            }
+        });
+
+        var imgStore = Ext.create('Ext.data.Store', {
+            fields: ['description', 'src'],
+            data: [{
+                description: 'WAT WAT WAT',
+                src: 'images/wat.jpg'
+            } , {
+                description: 'MEME',
+                src: 'images/meme.jpg'
+            } , {
+                description: 'TROLL MEME',
+                src: 'images/troll.png'
+            }]
+        });
+
+        var imgTpl = Ext.create('Ext.XTemplate', [
+            '<div>',
+                '<tpl for=".">',
+                    '<div class="listImg">',
+                        '<img src="{src}" style="width: 200px; height: 200px">',
+                        '<div class="description" style="display: none">{description}</div>',
+                    '</div>',
+                '</tpl>',
+            '</div>'
+        ]);
+
+        Ext.create('Ext.view.View', {
+            renderTo: Ext.getBody(),
+            itemSelector: 'div.listImg',
+            tpl: imgTpl,
+            store: imgStore,
+            listeners: {
+                itemclick: function (view, record) {
+                    var descriptionEl = view.getEl().down('.description');
+
+                    if (descriptionEl.isVisible()) descriptionEl.slideOut();
+                    else descriptionEl.slideIn();
                 }
             }
         });
