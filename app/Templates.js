308dd51585cb042768bac39bedef2e220f60496d
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-07-01 10:07:24 +0200

    added more examples

--- app/Templates.js
+++ app/Templates.js
@@ -56,6 +56,56 @@ Ext.define('SCE.Templates', {
                 }
             };
 
-        xtpl.overwrite(Ext.getBody(), data);
+        xtpl.append(Ext.getBody(), data);
+
+        var data = {
+            name: 'Wilk',
+            skills: {
+                PHP: 30,
+                Javascript: 100,
+                Python: 50
+            }
+        };
+
+        var xtpl2 = Ext.create('Ext.XTemplate', [
+            '<dl>',
+                '<tpl foreach="skills">',
+                    '<dd>{$}: {.}</dd>',
+                '</tpl>',
+            '</dl>'
+        ]);
+
+        xtpl2.append(Ext.getBody(), data);
+
+        var data2 = [{
+            name: 'wilk',
+            skills: ['PHP', 'Python', 'Javascript']
+        } , {
+            name: 'foo',
+            skills: ['Java', 'C', 'Go']
+        }];
+
+        var xtpl3 = Ext.create('Ext.XTemplate', [
+            '<div>',
+                '<tpl for=".">',
+                    '<div>{[values.name.toUpperCase() + " knows " + values.skills.join(", ")]}</div>',
+                '</tpl>',
+            '</div>'
+        ]);
+
+        xtpl3.append(Ext.getBody(), data2);
+
+        var langs = ['PHP', 'Python', 'Javascript', 'Ruby'];
+        var xtpl4 = Ext.create('Ext.XTemplate', [
+            '<div>',
+                '<b>List of my favourites programming languages:</b>',
+                '<tpl foreach=".">',
+                    '{% if (xkey === "PHP") continue; %}',
+                    '<div>{.}</div>',
+                '</tpl>',
+            '</div>'
+        ]);
+
+        xtpl4.append(Ext.getBody(), langs);
     }
 });
